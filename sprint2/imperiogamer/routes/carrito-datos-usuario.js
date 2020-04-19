5731fb62483887ee95a5fbbf07f7940bf970c53c
alejandro <alejandrodanielaquino@yahoo.com.ar>
2020-04-19 16:42:29 -0300

    Archivos eliminados. Código del carrito corregido

--- sprint2/imperiogamer/routes/carrito-datos-usuario.js
+++ sprint2/imperiogamer/routes/carrito-datos-usuario.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('carrito');
+  res.render('carrito-datos-usuario');
 });
 
 module.exports = router;
