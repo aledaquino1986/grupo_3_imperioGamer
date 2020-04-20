527dbb34b5399e2d56e3c6d02ff7d28124652800
lucasrivarola <lucasrivarola21@gmail.com>
2020-04-20 20:03:28 -0300

    agregue mi parte

--- sprint2/imperiogamer/routes/productos.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/detalle/', function(req, res, next) {
-  res.render('productos');
-});
-
-module.exports = router;
