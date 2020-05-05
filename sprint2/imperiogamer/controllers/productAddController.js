3d81199465e2a66aa0c239cfe8fbdbbc12d00553
Adrian5730 <Olaveves@gmail.com>
2020-05-05 02:15:45 -0300

    Carga de productos

--- sprint2/imperiogamer/controllers/productAddController.js
+++ sprint2/imperiogamer/controllers/productAddController.js
@@ -1,3 +1,8 @@
+const fs = require('fs')
+const path = require('path');
+const productosFilePath=path.join(__dirname,'../data/productos.json')
+let products = JSON.parse(fs.readFileSync(productosFilePath,{ encoding: 'utf-8' }))
+
 let productAddController = {
 
     /* Renderea página de carga de productos del admin */
@@ -5,8 +10,36 @@ let productAddController = {
     mostrarCargaProd: function(req, res, next) {
         res.render('productAdd');
 
-   }
+   },
 
+   cargaProducto:function(req, res, next){
+    
+        let nuevoProducto = {}
+        if( products == "" ){
+			nuevoProducto.id = 1
+		}else{
+			let ultimoProducto =  products[products.length-1]
+            nuevoProducto.id= ultimoProducto.id + 1
+           // console.log(ultimoProducto)
+		};
+        
+    
+        nuevoProducto.name = req.body.name,
+        nuevoProducto.discount = req.body.discount,
+        nuevoProducto.description = req.body.description,
+        nuevoProducto.codigo = req.body.codigo,
+        nuevoProducto.categoria = req.body.categoria,
+        nuevoProducto.price = req.body.price,
+        nuevoProducto.garantia = req.body.garantia,
+        nuevoProducto.color = req.body.color,
+        nuevoProducto.proveedor = req.body.proveedor,
+        nuevoProducto.image = req.file[0]
+        console.log(nuevoProducto)
+        products.push(nuevoProducto)
+        let productoSubirJSON = JSON.stringify(products)
+		fs.writeFileSync(productosFilePath,productoSubirJSON)
+        res.send(products)
+    }
 }
 
 module.exports = productAddController;
--- sprint2/imperiogamer/routes/productAdd.js
+++ sprint2/imperiogamer/routes/productAdd.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var productAddController = require("../controllers/productAddController");
+var path = require('path')
+var multer = require ('multer')
+var storage = multer.diskStorage({
+	destination:(req,file,cb)=>{
+		cb(null,'public/image/');
+	},
+	filename:(req,file,cb)=>{
+		cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+	}
+});
 
+var upload = multer({storage:storage});
 /* GET página de carga de productos del admin. */
 router.get('/', productAddController.mostrarCargaProd);
-
+router.post('/', upload.any(), productAddController.cargaProducto);
 module.exports = router;
