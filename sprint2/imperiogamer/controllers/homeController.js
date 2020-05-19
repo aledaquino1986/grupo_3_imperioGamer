dccc310355bcbc2cd9efecc301b7440fd2859ead
lucas rivarola <lucasrivarola21@gmail.com>
2020-05-19 18:52:32 -0300

    modifique el home

--- sprint2/imperiogamer/controllers/homeController.js
+++ sprint2/imperiogamer/controllers/homeController.js
@@ -3,13 +3,25 @@ const path = require('path');
 const productosFilePath=path.join(__dirname,'../data/productos.json')
 let products = JSON.parse(fs.readFileSync(productosFilePath,{ encoding: 'utf-8' }))
 
+
+
+
 let homeController = {
 
     /* Renderea el home. */
 
     mostrarHome: function(req, res, next) {
+      var populares = products.filter(function(producto){
+        return producto.codigo == "populares";
+      })
+      var ofertas = products.filter(function(producto){
+        return producto.codigo == "ofertas";
+      })
         res.render('index',{
-          title: "Imperio Gamer"
+          title: "Imperio Gamer",
+          productos: products,
+          populares: populares,
+          ofertas: ofertas
         });
 
    }
